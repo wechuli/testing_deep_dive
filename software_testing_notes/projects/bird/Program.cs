8d7e1d5736c5c6324f1159d7b1a5619ad7e3becb
Wechuli <wechulipaul@yahoo.com>
2020-07-29 22:12:52 +0300

    writing testable code

---
+++ software_testing_notes/projects/bird/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bird
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Bird
+    {
+        public int height = 0;
+        public int location = 0;
+
+        public Bird()
+        {
+
+        
+        }
+        public void Fly()
+        {
+            var rand = new Random();
+            height += rand.Next(10) + 1;
+            location += rand.Next(10) + 1;
+
+            Console.WriteLine($"{height}, {location}");
+        }
+    }
+}
